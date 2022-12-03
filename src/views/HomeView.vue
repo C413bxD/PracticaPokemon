ff5cbad1cb07028ded32acb2df56b534ec641eb6
C413bxD <caleb.895.9@gmail.com>
2022-12-02 21:13:18 -0500

    prueba de pinia composition-api api-options

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,15 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { useCounterStore } from "@/store/counter.js";
+import { storeToRefs } from "pinia";
+const useCounter = useCounterStore();
+const {increment} = useCounter
+const {count, double} = storeToRefs(useCounter)
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>Prueba de contador: {{ count }}</h1>
+    <h1>Doble: {{ double }}</h1>
+    <button @click="increment">increment</button>
+  </div>
 </template>
